f8109ab63d1af590ad985494db2dc5495a4752f2
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2020-07-24 15:03:44 -0300

    Primeira versão

---
+++ Artigos/exec.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+pdflatex -synctex=1 -interaction=nonstopmode  sectio1.tex
+pdflatex -synctex=1 -interaction=nonstopmode  sectio1.tex
+
+pdflatex -synctex=1 -interaction=nonstopmode  sectio2.tex
+pdflatex -synctex=1 -interaction=nonstopmode  sectio2.tex
+
+pdflatex -synctex=1 -interaction=nonstopmode  sectio3.tex
+pdflatex -synctex=1 -interaction=nonstopmode  sectio3.tex
+bibtex sectio3
+pdflatex -synctex=1 -interaction=nonstopmode  sectio3.tex
+pdflatex -synctex=1 -interaction=nonstopmode  sectio3.tex
+
+
+rm -f sectio*.aux  
+rm -f sectio*.log  
+rm -f sectio*.synctex.gz 
+rm -f *-converted-to.pdf
+rm -f sectio*.run.xml
+rm -f sectio*.blg
+rm -f sectio*.bbl
+rm -f sectio*-blx.bib
+rm -f mpxerr.tex
+
